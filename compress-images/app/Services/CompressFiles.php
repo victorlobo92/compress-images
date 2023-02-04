27e891f71516c8401f37b76b86ed39787b43a97c
Victor Lobo <victorlobo92@gmail.com>
2023-02-04 16:21:52 -0300

    Implement setup method to CompressFiles class

--- compress-images/app/Services/CompressFiles.php
+++ compress-images/app/Services/CompressFiles.php
@@ -4,12 +4,23 @@ namespace App\Services;
 
 class CompressFiles
 {
+    private $compress_folder_path;
+    private string $compressed_files_folder;
     private array $files_to_compress;
     private array $files_compressed = [];
 
     public function __construct(array $files_to_compress)
     {
         $this->files_to_compress = $files_to_compress;
+        $this->setup();
+    }
+
+    private function setup()
+    {
+        $compress_folder_path = trim(getenv('COMPRESS_FOLDER_PATH'), '/');
+
+        $this->compress_folder_path = ($compress_folder_path ? $compress_folder_path : base_path()) . '/';
+        $this->compressed_files_folder = trim(getenv('COMPRESSED_FILES_FOLDER'), '/') . '/';
     }
 
     /**
