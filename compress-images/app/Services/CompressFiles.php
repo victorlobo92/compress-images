ab90cad6d74344499859ef447596fb8549d5bd36
Victor Lobo <victorlobo92@gmail.com>
2023-02-04 16:43:10 -0300

    Implement validation method to CompressFiles class

--- compress-images/app/Services/CompressFiles.php
+++ compress-images/app/Services/CompressFiles.php
@@ -2,6 +2,9 @@
 
 namespace App\Services;
 
+use App\Exceptions\AccessRightsException;
+use App\Exceptions\FileOrFolderNotFoundException;
+use App\Exceptions\MissingEnvironmentVariableException;
 class CompressFiles
 {
     private $compress_folder_path;
@@ -13,6 +16,7 @@ class CompressFiles
     {
         $this->files_to_compress = $files_to_compress;
         $this->setup();
+        $this->validate();
     }
 
     private function setup()
@@ -23,6 +27,38 @@ class CompressFiles
         $this->compressed_files_folder = trim(getenv('COMPRESSED_FILES_FOLDER'), '/') . '/';
     }
 
+    private function validate()
+    {
+        $this->validate_environment_variables();
+        $this->validate_folders_exist();
+        $this->validate_folders_access_rights();
+    }
+
+    private function validate_environment_variables()
+    {
+        if (empty(trim(getenv('COMPRESSED_FILES_FOLDER'), '/'))) {
+            throw new MissingEnvironmentVariableException('Envirionment variable COMPRESSED_FILES_FOLDER is missing or invalid!');
+        }
+    }
+
+    private function validate_folders_exist()
+    {
+        if (!is_dir($this->get_compressed_files_path())) {
+            throw new FileOrFolderNotFoundException('The folder ' . $this->get_compressed_files_path() . ' was not found!');
+        }
+    }
+
+    private function validate_folders_access_rights()
+    {
+        if (!is_writable($this->get_compressed_files_path())) {
+            throw new AccessRightsException('The application does not have writing access rights to ' . $this->get_compressed_files_path() . ' folder!');
+        }
+    }
+
+    private function get_compressed_files_path()
+    {
+        return $this->compress_folder_path . $this->compressed_files_folder;
+    }
     /**
      * Compress files
      *
--- compress-images/tests/Unit/Services/CompressFilesTest.php
+++ compress-images/tests/Unit/Services/CompressFilesTest.php
@@ -2,26 +2,77 @@
 
 namespace Tests\Unit\Services;
 
-use App\Services\CompressFilesInterface;
+use App\Exceptions\AccessRightsException;
+use App\Exceptions\FileOrFolderNotFoundException;
+use App\Exceptions\MissingEnvironmentVariableException;
+use App\Services\Compress\CompressInterface;
 use App\Services\CompressFiles;
 use Tests\TestCase;
 
 class CompressFilesTest extends TestCase
 {
-    /**
-     * Test if CompressFiles class implements CompressFilesInterface
-     *
-     * @return void
-     */
-    public function test_class_implements_interface()
-    {
-        $compressFiles = new CompressFiles([]);
+    private $compress_mock;
 
-        $this->assertInstanceOf(CompressFilesInterface::class, $compressFiles);
+    /**
+     * @before
+     */
+    public function setup_shared_dependency(): void
+    {
+        $this->compress_mock = $this->createMock(CompressInterface::class);
     }
 
     /**
-     * Test if CompressFiles has attribute files_to_compress
+     * @after
+     */
+    public function tear_down_environment_changes()
+    {
+        putenv("COMPRESSED_FILES_FOLDER=" . env('COMPRESSED_FILES_FOLDER'));
+    }
+
+    /**
+     * Test if missing environment variable COMPRESSED_FILES_FOLDER throws exception
+     *
+     * @return void
+     */
+    public function test_missing_compressed_files_folder_env()
+    {
+        $this->expectException(MissingEnvironmentVariableException::class);
+
+        putenv("COMPRESSED_FILES_FOLDER=");
+        
+        new CompressFiles($this->compress_mock, []);
+    }
+
+    /**
+     * Test if exception is thrown when COMPRESSED_FILES_FOLDER directory does not exists
+     *
+     * @return void
+     */
+    public function test_compressed_folder_exists_exception()
+    {
+        $this->expectException(FileOrFolderNotFoundException::class);
+
+        putenv("COMPRESSED_FILES_FOLDER=fake_folder");
+        
+        new CompressFiles($this->compress_mock, []);
+    }
+
+    /**
+     * Test if application has COMPRESSED_FILES_FOLDER writing access rights
+     *
+     * @return void
+     */
+    public function test_writing_access_rights_to_compressed_folder()
+    {
+        $this->expectException(AccessRightsException::class);
+
+        putenv("COMPRESSED_FILES_FOLDER=tests/compress/unaccessable");
+
+        new CompressFiles($this->compress_mock, []);
+    }
+
+    /**
+     * Test if CompressFiles has attributes
      *
      * @return void
      */
