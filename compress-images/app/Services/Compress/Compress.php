5dc03cc086a6d41ab6a74c5e4710f3cc4bf7eb8c
Victor Lobo <victorlobo92@gmail.com>
2023-02-04 17:36:36 -0300

    Implement setup and validate methods

--- compress-images/app/Services/Compress/Compress.php
+++ compress-images/app/Services/Compress/Compress.php
@@ -2,8 +2,28 @@
 
 namespace App\Services\Compress;
 
+use Exception;
 
 abstract class Compress implements CompressInterface
 {
+    protected $file;
+    private string $file_destination_folder;
+    protected string $quality;
 
+    public function setup(array $file_to_compress, string $file_destination_folder)
+    {
+        $this->file = $file_to_compress;
+        $this->file_destination_folder = $file_destination_folder;
+
+        $this->validate();
+
+        return $this;
+    }
+
+    protected function validate()
+    {
+        if(empty($this->quality)){
+            throw new Exception('Please inform the quality!');
+        }
+    }
 }
--- compress-images/tests/Unit/Services/Compress/CompressTest.php
+++ compress-images/tests/Unit/Services/Compress/CompressTest.php
@@ -4,6 +4,7 @@ namespace Tests\Unit\Services\Compress;
 
 use App\Services\Compress\Compress;
 use Error;
+use Exception;
 use PHPUnit\Framework\TestCase;
 
 class CompressTest extends TestCase
@@ -18,5 +19,20 @@ class CompressTest extends TestCase
         $this->expectException(Error::class);
 
         new Compress();
+    }
+
+    /**
+     * Throw exception when property quality is empty
+     *
+     * @return void
+     */
+    public function test_exception_when_property_quality_is_empty()
+    {
+        $this->expectException(Exception::class);
+        $this->expectExceptionMessage('Please inform the quality!');
+
+        $compressMock = $this->createPartialMock(Compress::class, []);
+
+        $compressMock->setup([], '');
     }
 }
