b4674fb46dd8e28ec9f4695491bdd186749ae5c1
Victor Lobo <victorlobo92@gmail.com>
2023-02-04 17:52:46 -0300

    Implement folder validation and creation

--- compress-images/app/Services/Compress/Compress.php
+++ compress-images/app/Services/Compress/Compress.php
@@ -26,4 +26,41 @@ abstract class Compress implements CompressInterface
             throw new Exception('Please inform the quality!');
         }
     }
+
+    public function compress()
+    {
+        $this->make_dir_if_needed($this->get_file_destination_folder());
+
+        return $this->get_file_destination_path();
+    }
+
+    /**
+     * Make directory if it does not exist yet
+     *
+     * @param string $dir
+     * @return void
+     */
+    private function make_dir_if_needed($dir)
+    {
+        if(!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+    }
+
+    private function get_file_destination_folder()
+    {
+        $compress_folder_path = trim(getenv('COMPRESS_FOLDER_PATH'), '/');
+
+        if (!$compress_folder_path) $compress_folder_path = base_path();
+
+        $search_files_folder = $compress_folder_path . '/' . trim(getenv('SEARCH_FILES_FOLDER'), '/') . '/';
+        $destination_folder = str_replace($search_files_folder, '', $this->file['folder']);
+
+        return $this->file_destination_folder . $destination_folder;
+    }
+
+    protected function get_file_destination_path()
+    {
+        return $this->get_file_destination_folder() . $this->file['name'];
+    }
 }
--- compress-images/tests/Unit/Services/Compress/CompressTest.php
+++ compress-images/tests/Unit/Services/Compress/CompressTest.php
@@ -9,6 +9,42 @@ use PHPUnit\Framework\TestCase;
 
 class CompressTest extends TestCase
 {
+    private function get_base_path()
+    {
+        $base_path = trim(getenv('COMPRESS_FOLDER_PATH'), '/');
+
+        if (!$base_path) $base_path = base_path();
+
+        return $base_path;
+    }
+
+    public static function delTree($dir) {
+
+        $files = array_diff(scandir($dir), array('.','..'));
+     
+        foreach ($files as $file) {
+     
+            (is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
+     
+        }
+     
+        return rmdir($dir);
+     
+    }
+
+    /**
+     * @after
+     */
+    public function tear_down_environment_changes()
+    {
+        putenv("COMPRESSED_FILES_FOLDER=" . env('COMPRESSED_FILES_FOLDER'));
+
+        if (!empty($_SESSION['del_tree'])) {
+            self::delTree($_SESSION['del_tree']);
+            unset($_SESSION['del_tree']);
+        }
+    }
+
     /**
      * Throw exception when trying to instanciate abstract class directly
      *
@@ -46,5 +82,36 @@ class CompressTest extends TestCase
         $compressMock = $this->createPartialMock(Compress::class, ['validate']);
         $compressMock->expects($this->once())->method('validate');
         $compressMock->setup([], '');
+    }
+
+    /**
+     * Test mkdir is working propertly
+     *
+     * @return void
+     */
+    public function test_mkdir_is_working_propertly()
+    {
+        $search_file_folder = $this->get_base_path() . '/' . trim(getenv('SEARCH_FILES_FOLDER'), '/');
+        
+        $file_to_compress = [
+            'folder' => "$search_file_folder/accessable/",
+            'name' => 'dog_2.png'
+        ];
+
+        $compressed_files_folder = trim(getenv('COMPRESSED_FILES_FOLDER'), '/') . '/';
+
+        $test_mkdir_folder = $compressed_files_folder . 'test_mkdir';
+
+        putenv("COMPRESSED_FILES_FOLDER=$test_mkdir_folder");
+
+        $compressed_files_folder = $this->get_base_path() . '/' . $compressed_files_folder;
+
+        $compressMock = $this->createPartialMock(Compress::class, ['validate']);
+        $compressMock->setup($file_to_compress, $test_mkdir_folder . '/');
+        $compressMock->compress();
+        
+        $_SESSION['del_tree'] = $test_mkdir_folder;
+
+        $this->assertTrue(is_dir($compressed_files_folder));
     }
 }
