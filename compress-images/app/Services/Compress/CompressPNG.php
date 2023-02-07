c3e5dd9cfdf746299171e541a62e3b972d446185
Victor Lobo <victorlobo92@gmail.com>
2023-02-07 11:59:38 -0300

    Implement compress_file method to CompressPNG class and it's tests

--- compress-images/app/Services/Compress/CompressPNG.php
+++ compress-images/app/Services/Compress/CompressPNG.php
@@ -4,4 +4,22 @@ namespace App\Services\Compress;
 
 class CompressPNG extends Compress
 {
+    public function __construct()
+    {
+        $this->quality = (!empty($this->quality)) ? $this->quality : 9 ;
+    }
+
+    /**
+     * Compress single PNG file
+     *
+     */
+    protected function compress_file($temp_file_name)
+    {
+        $file_compressed = imagecreatefrompng($this->file['path']);
+        imagealphablending($file_compressed , false);
+        imagesavealpha($file_compressed , true);
+        imagepng($file_compressed, $temp_file_name, $this->quality);
+
+        return $file_compressed;
+    }
 }
--- compress-images/tests/Unit/Services/Compress/CompressPNGTest.php
+++ compress-images/tests/Unit/Services/Compress/CompressPNGTest.php
@@ -8,6 +8,40 @@ use Tests\TestCase;
 
 class CompressPNGTest extends TestCase
 {
+    private function get_base_path()
+    {
+        $base_path = trim(getenv('COMPRESS_FOLDER_PATH'), '/');
+
+        if (!$base_path) $base_path = base_path();
+
+        return $base_path;
+    }
+
+    public static function delTree($dir) {
+
+        $files = array_diff(scandir($dir), array('.','..'));
+     
+        foreach ($files as $file) {
+     
+            (is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
+     
+        }
+     
+        return rmdir($dir);
+     
+    }
+
+    /**
+     * @after
+     */
+    public function tear_down_environment_changes()
+    {
+        if (!empty($_SESSION['del_tree'])) {
+            self::delTree($_SESSION['del_tree']);
+            unset($_SESSION['del_tree']);
+        }
+    }
+
     /**
      * Test if Compress class implements CompressInterface
      *
@@ -33,5 +67,32 @@ class CompressPNGTest extends TestCase
         $this->expectNotToPerformAssertions();
 
         new CompressPNG();
+    }
+
+    /**
+     * Method compress_file has been called from child class
+     *
+     * @return void
+     */
+    public function test_method_compress_file_has_been_called_from_child_class()
+    {
+        $search_file_folder = $this->get_base_path() . '/' . trim(getenv('SEARCH_FILES_FOLDER'), '/');
+
+        $file_to_compress = [
+            'folder' => "$search_file_folder/accessable/",
+            'name' => 'dog_2.png'
+        ];
+
+        $file_to_compress['path'] = $file_to_compress['folder'] . $file_to_compress['name'];
+
+        $compressed_files_folder = $this->get_base_path() . '/' . trim(getenv('COMPRESSED_FILES_FOLDER'), '/') . '/';
+
+        $file_destionation_path = $compressed_files_folder . 'accessable/dog_2.png';
+
+        $compressPNG = new CompressPNG();
+        $compressPNG->setup($file_to_compress, $compressed_files_folder);
+        $this->assertEquals($file_destionation_path, $compressPNG->compress());
+
+        $_SESSION['del_tree'] = $compressed_files_folder . 'accessable';
     }
 }
